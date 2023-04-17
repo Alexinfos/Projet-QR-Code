3716b3a39af3c986193f8f66a50fec700e235c90
alexisbn <alexinfos@free.fr>
2023-04-17 15:51:26 +0200

    Update display.py

--- display.py
+++ display.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def show_matrix(M):
+def show_matrix(M, palette='binary'):
     # Affiche l'image seule
-    plt.imshow(M, cmap=plt.get_cmap('binary'))
+    plt.imshow(M, cmap=plt.get_cmap(palette))
     plt.show()
 
 # Tests
